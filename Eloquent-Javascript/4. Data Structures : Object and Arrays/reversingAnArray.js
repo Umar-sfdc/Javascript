ef66cd80a26f6b5ef9defc71138b4294be5f295d
Mohammad Umar <mohammad.umar.sfdc@gmail.com>
2025-02-23 16:33:42 +0530

    Implemented reversingAnArray.js - reverse the existing array & generates new reverse array.

--- Eloquent-Javascript/4. Data Structures : Object and Arrays/reversingAnArray.js
+++ Eloquent-Javascript/4. Data Structures : Object and Arrays/reversingAnArray.js
@@ -1,1 +1,34 @@
+/* 
+
+============= REVERSING AN ARRAY ================
+
+Q) Arrays have a reverse method that changes the array by inverting the order in which its elements appear. For this exercise, write two functions, reverseArray and reverseArrayInPlace. The first, reverseArray, should take an array as its argument and produce a new array that has the same elements in the inverse order. The second, reverseArrayInPlace, should do what the reverse method does: modify the array given as its argument by reversing its elements. Neither may use the standard reverse method.
+
+
+*/
+
+function reverseArray(array) {
+  let arr = [];
+
+  for (let item of array) {
+    arr.unshift(item);
+  }
+  return arr;
+}
+
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let old = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = old;
+  }
+  return array;
+}
+
+let myArray = ["A", "B", "C"];
+console.log(reverseArray(myArray));
+// → ["C", "B", "A"];
+
+console.log(myArray);
+console.log("Reverse Array : ", reverseArrayInPlace(myArray));
 
